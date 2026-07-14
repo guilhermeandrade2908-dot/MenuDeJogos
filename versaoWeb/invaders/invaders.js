ba7a0286504c708104be8147f45ce95270bc0ec9
Guill <guilhermeandrade2908@gmail.com>
2026-07-14 19:39:46 -0300

    Atualiza a lógica do jogo Space Invaders, corrigindo o ID do canvas e implementando a movimentação da nave, além de adicionar a função de desenho da nave e o loop principal do jogo.

--- versaoWeb/invaders/invaders.js
+++ versaoWeb/invaders/invaders.js
@@ -1,11 +1,11 @@
 // VARIÁVEIS PARA CAPTURA DOS ELEMENTOS
-const canvas = document.getElementById("spaceInvaders");
+const canvas = document.getElementById("spaceCanvas");
 const context = canvas.getContext('2d');
 const btnBack = document.getElementById("btn-back-menu");
 const htmlScore = document.getElementById("score-text");
 const htmlLives = document.getElementById("lives-text");
 
-// CONFIGURAÇÃO DO BOTÃO DE VOLTAR 
+// CONFIGURAÇÃO DO BOTÃO DE VOLTAR PARA O MENU
 btnBack.addEventListener('click', () => {
     window.location.href = "../hub/index.html";
 });
@@ -44,4 +44,62 @@ window.addEventListener("keyup", (evento) => { // ESCUTA QUANDO A TECLA É SOLTA
     if (evento.key in teclas) {
         teclas[evento.key] = false;
     }
-})
+});
+
+// ATUALIZADORES E FÍSICA DO JOGO
+function atualizarMovimento() {
+    // SE A SETA DA ESQUERDA OU A TECLA 'A' ESTIVERAM PRESSIONADAS:
+    if (teclas.arrowLeft || teclas.a) {
+        player.x -= player.velocidade;
+    }
+    // SE A SETA DIREITA OU A TECLA 'D' ESTIVERAM PRESSIONADAS: 
+    if (teclas.arrowRight || teclas.d) {
+        player.x += player.velocidade;
+    }
+
+    // LIMITADORES DE BORDAS DO JOGO
+    if (player.x < 10) {
+        player.x = 10;
+    }
+    if (player.x > canvas.width - naveWidth - 10) {
+        player.x = canvas.width - naveWidth - 10;
+    }
+};
+
+// FUNÇÃO DE DESENHO DO JOGO:
+function desenharJogo() {
+    // LIMPA A TELA PARA O PRÓXIMO FRAME:
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // DESENHA A NAVE DO PLAYER 
+    context.fillStyle = player.cor;
+    context.shadowBlur = 15;
+    context.shadowColor = player.cor;
+
+    // BASE DA NAVE
+    context.fillRect(player.x, player.y, naveWidth, naveHeight);
+
+    // CANO DO CANHÃO CENTRALIZADOR
+    context.fillRect(
+        player.x + naveWidth / 2 - 4, // ALINHA NO MEIO DA BASE
+        player.y - 8, // SOBE UM POUCO
+        8, // LARGURA DO CANHÃO
+        8 // ALTURA DO CANHÃO
+    );
+
+    // RESETA O EFEITO DE SOMBRA PARA NÃO IMPACTAR OUTRAS COISAS
+    context.shadowBlur = 0;
+}
+
+// LOOP PRINCIPAL DO JOGO
+function gameLoop() {
+    if (!jogoFinalizado) {
+        atualizarMovimento();
+    }
+
+    desenharJogo();
+    requestAnimationFrame(gameLoop);
+}
+
+// INICIA O JOGO
+gameLoop();
